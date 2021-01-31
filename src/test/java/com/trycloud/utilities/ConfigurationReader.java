0ca07720ecda3adc54305b71737da710945b7ae4
AnaKeane <74145178+AnaKeane@users.noreply.github.com>
2021-01-31 13:30:32 -0600

    configuration reader comment out

--- src/test/java/com/trycloud/utilities/ConfigurationReader.java
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
-    // this is
+
     private static Properties properties = new Properties();
 
     static {
