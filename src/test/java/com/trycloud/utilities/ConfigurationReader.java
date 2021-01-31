3b7e6451d0a06b68c340008a055cc1fb048bc496
AnaKeane <74145178+AnaKeane@users.noreply.github.com>
2021-01-31 10:10:25 -0600

    configuration reader is done

--- src/test/java/com/trycloud/utilities/ConfigurationReader.java
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -1,4 +1,27 @@
 package com.trycloud.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
+    private static Properties properties=new Properties();
+    static {
+
+        FileInputStream file= null;
+        try {
+            file = new FileInputStream("configuration.properties");
+            properties.load(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public static String getProperty(String keyWord){
+        return properties.getProperty(keyWord);
+    }
+
 }
