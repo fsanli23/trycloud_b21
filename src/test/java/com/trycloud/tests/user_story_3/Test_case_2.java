650f4c288c9dbc95e4413448fd3ed0aa8dfe8215
fsanli23 <sanlifatih23@gmail.com>
2021-02-02 02:40:06 -0600

    test case 2 added for user story 3

--- src/test/java/com/trycloud/tests/user_story_3/Test_case_2.java
+++ src/test/java/com/trycloud/tests/user_story_3/Test_case_2.java
@@ -1,6 +1,45 @@
 package com.trycloud.tests.user_story_3;
 
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Test_case_2 {
+
+    WebDriver driver;
+    @BeforeClass
+    public void login(){
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://qa.trycloud.net/index.php/login?clear=1");
+        driver.manage().window().maximize();
+
+
+
+    }
+    @Test
+    public void loginPage(){
+
+        driver.findElement(By.id("user")).sendKeys("User21");
+        driver.findElement(By.id("password")).sendKeys("Userpass123");
+        driver.findElement(By.id("submit-wrapper")).click();
+    }
+@Test
+public  void allBoxesSelected() throws InterruptedException {
+
+    WebElement allbutton= driver.findElement(By.xpath("//input[@id='select_all_files']"));
+    allbutton.click();
+
+
+
+
+}
+
+
+
 
 
 }
