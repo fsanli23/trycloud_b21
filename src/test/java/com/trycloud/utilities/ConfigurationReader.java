6555a40d312a749ef90b97fd3abd96f4e85bb24a
fsanli23 <sanlifatih23@gmail.com>
2021-02-01 21:40:33 -0600

    try1

--- src/test/java/com/trycloud/utilities/ConfigurationReader.java
+++ src/test/java/com/trycloud/utilities/ConfigurationReader.java
@@ -28,22 +28,4 @@ public class ConfigurationReader {
 
 
 
-    private static Properties properties = new Properties();
-
-    static {
-        try {
-            FileInputStream file = new FileInputStream("configuration.properties");
-            properties.load(file);
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static String getProperty(String keyWord) {
-        return properties.getProperty(keyWord);
-
-
-    }
 }
