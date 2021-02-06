6bf87c6148c4c3695c91aab74284d959662b0127
fsanli23 <sanlifatih23@gmail.com>
2021-02-05 18:02:44 -0600

    test case 2 added for user story 4

--- src/test/java/com/trycloud/tests/user_story_3/Test_case_3.java
+++ src/test/java/com/trycloud/tests/user_story_3/Test_case_3.java
@@ -11,9 +11,35 @@ import java.util.concurrent.TimeUnit;
 
 public class Test_case_3 {
 
+    WebDriver driver;
+
+    @BeforeClass
+    public void login() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.get("http://qa.trycloud.net/index.php/login?clear=1");
+        driver.manage().window().maximize();
 
 
+    }
 
+    @Test
+    public void loginPage() {
+
+        driver.findElement(By.id("user")).sendKeys("User21");
+        driver.findElement(By.id("password")).sendKeys("Userpass123");
+        driver.findElement(By.id("submit-wrapper")).click();
+    }
+
+@Test
+    public void addToFavorites() {
+
+        WebElement actionButton = driver.findElement(By.xpath("(//a[@class='action action-menu permanent'])[2]"));
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        actionButton.click();
+
+
+    }
 
 
 }
